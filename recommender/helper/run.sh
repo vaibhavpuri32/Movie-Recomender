f5ec1b033c06b27d9d38eff06410696b25c7e3e0
vaibhav <vaibhavpuri32@gmail.com>
2020-05-18 12:01:14 +0530

    First commit

---
+++ recommender/helper/run.sh
@@ -0,0 +1,4 @@
+python3 /home/yogesh/WTA/movie_recommendation/recommender/trainer/test.py
+g++ /home/yogesh/WTA/movie_recommendation/recommender/trainer/process.cpp
+/home/yogesh/WTA/movie_recommendation/recommender/trainer/a.out
+
